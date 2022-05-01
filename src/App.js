07194f167b71a409e8ce31ca66d5967d3f25d2dd
abuiliazeed <abuiliazeed@gmail.com>
2022-05-01 12:25:25 +0200

    adding cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Analytics from './components/Analytics';
 import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
 
     </div>
   )
