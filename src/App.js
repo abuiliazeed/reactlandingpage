0a39e5df22e3fb7a052512cdc701798bd5ca607e
abuiliazeed <abuiliazeed@gmail.com>
2022-05-01 12:57:27 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Analytics from './components/Analytics';
 import Newsletter from './components/Newsletter';
 import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 
 
@@ -16,7 +17,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
-
+      <Footer />
     </div>
   )
 }
